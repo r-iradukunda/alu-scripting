0b0ef81be880589cdec2e46e95f224a1cc44f2cf
r-iradukunda <r.iradukund@alustudent.com>
2023-11-09 08:26:16 -0800

    Recurse it

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-""" recurse it"""
 import requests
 
-
 def recurse(subreddit, hot_list=[], after=None):
-    """ returns list with titles of all hot articles in a subreddit """
+    """ returns a list with titles of all hot articles in a subreddit """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     params = {'after': after}
     response = requests.get(
-                                url,
-                                headers=headers,
-                                params=params,
-                                allow_redirects=False
-                            )
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False
+    )
     if response.status_code == 200:
         data = response.json().get('data')
         if data is not None:
@@ -21,6 +19,8 @@ def recurse(subreddit, hot_list=[], after=None):
             if children is not None:
                 for child in children:
                     hot_list.append(child.get('data').get('title'))
+                    if len(hot_list) >= 2:  # Limit the number of titles to 2
+                        return hot_list
                 after = data.get('after')
                 if after is not None:
                     return recurse(subreddit, hot_list, after)
